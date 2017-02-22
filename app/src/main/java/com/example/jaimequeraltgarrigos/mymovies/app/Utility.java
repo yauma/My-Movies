8b38ce89118db0a5f99d6c767d9d7689f1f87254
QUERJ009 <Jaime.X.Queralt.-ND@disney.com>
2017-02-22 11:31:20 -0300

    GridLayout Manager Fit

--- app/src/main/java/com/example/jaimequeraltgarrigos/mymovies/app/Utility.java
+++ app/src/main/java/com/example/jaimequeraltgarrigos/mymovies/app/Utility.java
@@ -11,7 +11,7 @@ public class Utility {
     public static int calculateNoOfColumns(Context context) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
-        int noOfColumns = (int) (dpWidth / 120);
+        int noOfColumns = (int) (dpWidth / 180);
         return noOfColumns;
     }
 }
